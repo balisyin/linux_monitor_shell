d163937018b8673293f780f130a00e3f8b32af0e
殷小林 <yinxiaolin@inke.cn>
2020-02-24 14:10:05 +0800

    servie

---
+++ serviceMysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+fileName="info.txt"
+MYSQL='mysql -h -P3306 -u -p -Dserviceinfo_chatright'#fixme
+
+if [ ! -f $fileName ]; then
+    echo "info.txt not exist"
+fi
+
+cat $fileName | while read line
+do
+    name=`echo $line |awk '{print $1}'`
+    value=`echo $line | awk '{print $2}'`
+
+    cmd="insert into serviceinfo_server_policy (server_name,server_url,https_server_url,server_type,switch,collection) values (\"$name\",\"$value\",\"$value\", 1,1,1);"
+    echo $cmd
+    #echo $cmd | `$MYSQL`
+done
