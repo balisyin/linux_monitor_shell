f939038249efc6c550b4eeae8e9dc839fcafcf29
殷小林 <yinxiaolin@inke.cn>
2020-03-22 15:47:39 +0800

    log

--- serviceMysql.sh
+++ serviceMysql.sh
@@ -11,7 +11,8 @@ cat $fileName | while read line
 do
     name=`echo $line |awk '{print $1}'`
     value=`echo $line | awk '{print $2}'`
-
+    #value=`echo $line | awk '{print $2}' | sed 's/http/https/g' | sed 's/test//g'`
+   # echo $name $value 
     cmd="insert into serviceinfo_server_policy (server_name,server_url,https_server_url,server_type,switch,collection) values (\"$name\",\"$value\",\"$value\", 1,1,1);"
     echo $cmd
     #echo $cmd | `$MYSQL`
